9926678e6cb1472c632e51dbf6c5b59e1f64c792
wangy <wangyanjava@gmail.com>
2017-11-03 17:51:28 +0800

    1

--- js/api/PZ_zhifajg.js
+++ js/api/PZ_zhifajg.js
@@ -1,57 +1,66 @@
-$.getJSON("http://60.205.186.12:8080/API/ZfjgXkjgAction?choice=ZfjgXkjgList", function(data) {
+$.getJSON("http://60.205.186.12:8080/API/ZfjgXkjgAction?choice=list", function(data) {
     $("#table1 tbody").html(""); //清空
-    var htmlText = "<tr>";
+    var htmlText = "";
+
     //for in循环
     for(x in data){
         //x表示是下标，来指定变量，指定的变量可以是数组元素，也可以是对象的属性。
-        console.log(data[x].name);
+        console.log(data[x].title);
         console.log(data[x].content);
+        htmlText += "<tr>";
         htmlText += "<td>" + data[x].id +"</td>";
-        htmlText += "<td width='30%'>" + data[x].name +"</td>";
-        htmlText += "<td>" + data[x].content.substring(0,30) +"...<a href='http://60.205.186.12:8080/API/ZfjgXkjgAction?choice=ByldZfjgXkjg&Id="+data[x].id+"'>详细</a></td>";
+        htmlText += "<td width='30%'>" + data[x].title +"</td>";
+        htmlText += "<td>" + data[x].content.substring(0,10) +"...<a href=''>详细</a></td>";
         htmlText += "<td> <div class='btn-group'> " +
             "<button type='button' class='btn-primary btn btn-xs'>编辑</button> " +
             "<button type='button' class='btn-danger btn btn-xs'>删除</button> " +
             "</div> </td>";
+        htmlText += "</tr>";
     }
-    htmlText += "</tr>";
+
     $("#table1 tbody").html(htmlText);
 });
-$.getJSON("http://60.205.186.12:8080/API/ZfjgCfjgAction?choice=ZfjgCfjgList", function(data) {
+$.getJSON("http://60.205.186.12:8080/API/ZfjgCfjgAction?choice=list", function(data) {
     $("#table2 tbody").html(""); //清空
-    var htmlText = "<tr>";
+    var htmlText = "";
+
     //for in循环
     for(x in data){
         //x表示是下标，来指定变量，指定的变量可以是数组元素，也可以是对象的属性。
-        console.log(data[x].name);
+        console.log(data[x].title);
         console.log(data[x].content);
+        htmlText += "<tr>";
         htmlText += "<td>" + data[x].id +"</td>";
-        htmlText += "<td width='30%'>" + data[x].name +"</td>";
-        htmlText += "<td>" + data[x].content.substring(0,30) +"...<a href='http://60.205.186.12:8080/API/ZfjgCfjgAction?choice=ByldZfjgCfjg&Id="+data[x].id+"'>详细</a></td>";
+        htmlText += "<td width='30%'>" + data[x].title +"</td>";
+        htmlText += "<td>" + data[x].content.substring(0,10) +"...<a href=''>详细</a></td>";
         htmlText += "<td> <div class='btn-group'> " +
             "<button type='button' class='btn-primary btn btn-xs'>编辑</button> " +
             "<button type='button' class='btn-danger btn btn-xs'>删除</button> " +
             "</div> </td>";
+        htmlText += "</tr>";
     }
-    htmlText += "</tr>";
+
     $("#table2 tbody").html(htmlText);
 });
-$.getJSON("http://60.205.186.12:8080/API/ZfjgJcjgAction?choice=ZfjgJcjgList", function(data) {
+$.getJSON("http://60.205.186.12:8080/API/ZfjgJcjgAction?choice=list", function(data) {
     $("#table3 tbody").html(""); //清空
-    var htmlText = "<tr>";
+    var htmlText = "";
+
     //for in循环
     for(x in data){
         //x表示是下标，来指定变量，指定的变量可以是数组元素，也可以是对象的属性。
-        console.log(data[x].name);
+        console.log(data[x].title);
         console.log(data[x].content);
+        htmlText += "<tr>";
         htmlText += "<td>" + data[x].id +"</td>";
-        htmlText += "<td width='30%'>" + data[x].name +"</td>";
-        htmlText += "<td>" + data[x].content.substring(0,30) +"...<a href='http://60.205.186.12:8080/API/ZfjgJcjgAction?choice=ByldZfjgJcjg&Id="+data[x].id+"'>详细</a></td>";
+        htmlText += "<td width='30%'>" + data[x].title +"</td>";
+        htmlText += "<td>" + data[x].content.substring(0,10) +"...<a href=''>详细</a></td>";
         htmlText += "<td> <div class='btn-group'> " +
             "<button type='button' class='btn-primary btn btn-xs'>编辑</button> " +
             "<button type='button' class='btn-danger btn btn-xs'>删除</button> " +
             "</div> </td>";
+        htmlText += "</tr>";
     }
-    htmlText += "</tr>";
+
     $("#table3 tbody").html(htmlText);
 });
