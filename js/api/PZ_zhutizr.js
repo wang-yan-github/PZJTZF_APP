f8888a481d6d783feec0ecb14c2f800a48c1a725
wangy <wangyanjava@gmail.com>
2017-11-04 19:01:08 +0800

    1

--- js/api/PZ_zhutizr.js
+++ js/api/PZ_zhutizr.js
@@ -1,3 +1,25 @@
+Date.prototype.format = function(fmt) {
+    var o = {
+        "M+" : this.getMonth()+1,                 //月份
+        "d+" : this.getDate(),                    //日
+        "h+" : this.getHours(),                   //小时
+        "m+" : this.getMinutes(),                 //分
+        "s+" : this.getSeconds(),                 //秒
+        "q+" : Math.floor((this.getMonth()+3)/3), //季度
+        "S"  : this.getMilliseconds()             //毫秒
+    };
+    if(/(y+)/.test(fmt)) {
+        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    }
+    for(var k in o) {
+        if(new RegExp("("+ k +")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        }
+    }
+    return fmt;
+}
+var date = new Date().format("yyyy-MM-dd hh:mm:ss");
+// 列表
 $.getJSON("http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=list", function(data) {
     $("#table1 tbody").html(""); //清空
     var htmlText = "";
@@ -198,4 +220,93 @@ $.getJSON("http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=list", function
 
     $("#table8 tbody").html(htmlText);
 });
+// 添加
+function s1() {
+    var si1 = $("#si1").val();
+    var si2 = $("#si2 .note-editable").text();
+    var si3 = $("#si3").val();
+    var si4 = $("#si4").val();
+    var si5 = $("#si5").val();
+
+    var url = "http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=save&title="+si1+"&content="+si2+"&date="+date+"&person=管理员&department="+si3+"&quality="+si4+"&people="+si5+"";
+    alert(url);
+    $.getJSON(url, function(data) {})
+}
+function s2() {
+    var si1 = $("#si1").val();
+    var si2 = $("#si2 .note-editable").text();
+    var si3 = $("#si3").val();
+    var si4 = $("#si4").val();
+    var si5 = $("#si5").val();
+
+    var url = "http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=save&title="+si1+"&content="+si2+"&date="+date+"&person=管理员&department="+si3+"&quality="+si4+"&people="+si5+"";
+    alert(url);
+    $.getJSON(url, function(data) {})
+}
+function s3() {
+    var si1 = $("#si1").val();
+    var si2 = $("#si2 .note-editable").text();
+    var si3 = $("#si3").val();
+    var si4 = $("#si4").val();
+    var si5 = $("#si5").val();
+
+    var url = "http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=save&title="+si1+"&content="+si2+"&date="+date+"&person=管理员&department="+si3+"&quality="+si4+"&people="+si5+"";
+    alert(url);
+    $.getJSON(url, function(data) {})
+}
+function s4() {
+    var si1 = $("#si1").val();
+    var si2 = $("#si2 .note-editable").text();
+    var si3 = $("#si3").val();
+    var si4 = $("#si4").val();
+    var si5 = $("#si5").val();
+
+    var url = "http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=save&title="+si1+"&content="+si2+"&date="+date+"&person=管理员&department="+si3+"&quality="+si4+"&people="+si5+"";
+    alert(url);
+    $.getJSON(url, function(data) {})
+}
+function s5() {
+    var si1 = $("#si1").val();
+    var si2 = $("#si2 .note-editable").text();
+    var si3 = $("#si3").val();
+    var si4 = $("#si4").val();
+    var si5 = $("#si5").val();
+
+    var url = "http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=save&title="+si1+"&content="+si2+"&date="+date+"&person=管理员&department="+si3+"&quality="+si4+"&people="+si5+"";
+    alert(url);
+    $.getJSON(url, function(data) {})
+}
+function s6() {
+    var si1 = $("#si1").val();
+    var si2 = $("#si2 .note-editable").text();
+    var si3 = $("#si3").val();
+    var si4 = $("#si4").val();
+    var si5 = $("#si5").val();
+
+    var url = "http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=save&title="+si1+"&content="+si2+"&date="+date+"&person=管理员&department="+si3+"&quality="+si4+"&people="+si5+"";
+    alert(url);
+    $.getJSON(url, function(data) {})
+}
+function s7() {
+    var si1 = $("#si1").val();
+    var si2 = $("#si2 .note-editable").text();
+    var si3 = $("#si3").val();
+    var si4 = $("#si4").val();
+    var si5 = $("#si5").val();
+
+    var url = "http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=save&title="+si1+"&content="+si2+"&date="+date+"&person=管理员&department="+si3+"&quality="+si4+"&people="+si5+"";
+    alert(url);
+    $.getJSON(url, function(data) {})
+}
+function s8() {
+    var si1 = $("#si1").val();
+    var si2 = $("#si2 .note-editable").text();
+    var si3 = $("#si3").val();
+    var si4 = $("#si4").val();
+    var si5 = $("#si5").val();
+
+    var url = "http://60.205.186.12:8080/API/ZtzdJtgczlAction?choice=save&title="+si1+"&content="+si2+"&date="+date+"&person=管理员&department="+si3+"&quality="+si4+"&people="+si5+"";
+    alert(url);
+    $.getJSON(url, function(data) {})
+}
 
